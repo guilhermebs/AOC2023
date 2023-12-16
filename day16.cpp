516fa8ebba31c4bfae151aba332d68479865d083
Guilherme Saturnino <guilherme.bicalho2@gmail.com>
2023-12-16 21:30:00 +0100

    use bitset

--- day16.cpp
+++ day16.cpp
@@ -12,10 +12,17 @@ inline void hash_combine(std::size_t & s, const T & v)
   s^= h(v) + 0x9e3779b9 + (s<< 6) + (s>> 2);
 }
 
+enum Direction {
+    up,
+    down,
+    left,
+    right
+};
+
 struct BeamFront {
     int i;
     int j;
-    char dir;
+    Direction dir;
 
     bool operator==(const BeamFront &other) const {
        return (
@@ -43,65 +50,65 @@ struct std::hash<BeamFront> {
 };
 
 std::vector<BeamFront> descendents(std::vector<std::string> *cave, BeamFront bf) {
-    BeamFront right = BeamFront{bf.i + 1, bf.j, '>'};
-    BeamFront left = BeamFront{bf.i - 1, bf.j, '<'};
-    BeamFront up = BeamFront{bf.i, bf.j - 1, '^'};
-    BeamFront down = BeamFront{bf.i, bf.j +1, 'v'};
+    BeamFront right = BeamFront{bf.i + 1, bf.j, Direction::right};
+    BeamFront left = BeamFront{bf.i - 1, bf.j, Direction::left};
+    BeamFront up = BeamFront{bf.i, bf.j - 1, Direction::up};
+    BeamFront down = BeamFront{bf.i, bf.j +1, Direction::down};
     char cur_cel = (*cave)[bf.j][bf.i];
 
-    if ((cur_cel == '.' || cur_cel == '-') && bf.dir == '>')
+    if ((cur_cel == '.' || cur_cel == '-') && bf.dir == Direction::right)
     {
         return std::vector<BeamFront>{right};
     }
-    else if ((cur_cel == '.' || cur_cel == '-') && bf.dir == '<')
+    else if ((cur_cel == '.' || cur_cel == '-') && bf.dir == Direction::left)
     {
         return std::vector<BeamFront>{left};
     }
-    else if ((cur_cel == '.' || cur_cel == '|') && bf.dir == '^')
+    else if ((cur_cel == '.' || cur_cel == '|') && bf.dir == Direction::up)
     {
         return std::vector<BeamFront>{up};
     }
-    else if ((cur_cel == '.' || cur_cel == '|') && bf.dir == 'v')
+    else if ((cur_cel == '.' || cur_cel == '|') && bf.dir == Direction::down)
     {
         return std::vector<BeamFront>{down};
     }
-    else if (cur_cel == '/' && bf.dir == '>')
+    else if (cur_cel == '/' && bf.dir == Direction::right)
     {
         return std::vector<BeamFront>{up};
     }
-    else if (cur_cel == '/' && bf.dir == '<')
+    else if (cur_cel == '/' && bf.dir == Direction::left)
     {
         return std::vector<BeamFront>{down};
     }
-    else if (cur_cel == '/' && bf.dir == '^')
+    else if (cur_cel == '/' && bf.dir == Direction::up)
     {
         return std::vector<BeamFront>{right};
     }
-    else if (cur_cel == '/' && bf.dir == 'v')
+    else if (cur_cel == '/' && bf.dir == Direction::down)
     {
         return std::vector<BeamFront>{left};
     }
-    else if (cur_cel == '\\' && bf.dir == '>')
+    else if (cur_cel == '\\' && bf.dir == Direction::right)
     {
         return std::vector<BeamFront>{down};
     }
-    else if (cur_cel == '\\' && bf.dir == '<')
+    else if (cur_cel == '\\' && bf.dir == Direction::left)
     {
         return std::vector<BeamFront>{up};
     }
-    else if (cur_cel == '\\' && bf.dir == '^')
+    else if (cur_cel == '\\' && bf.dir == Direction::up)
     {
         return std::vector<BeamFront>{left};
     }
-    else if (cur_cel == '\\' && bf.dir == 'v')
+    else if (cur_cel == '\\' && bf.dir == Direction::down)
     {
         return std::vector<BeamFront>{right};
     }
-    else if (cur_cel == '|' && (bf.dir == '>' || bf.dir == '<') )
+    else if (cur_cel == '|' && (bf.dir == Direction::right || bf.dir == Direction::left) )
     {
         return std::vector<BeamFront>{down, up};
     }
-    else if (cur_cel == '-' && (bf.dir == '^' || bf.dir == 'v') )
+    else if (cur_cel == '-' && (bf.dir == Direction::up || bf.dir == Direction::down) )
     {
         return std::vector<BeamFront>{left, right};
     }
@@ -110,26 +117,26 @@ std::vector<BeamFront> descendents(std::vector<std::string> *cave, BeamFront bf)
 
 size_t propagate_beam(std::vector<std::string> *cave, BeamFront start) {
     std::deque<BeamFront> beam_fronts{start};
-    std::unordered_set<BeamFront> seen;
+    std::vector<std::bitset<4>> seen;
     auto const nrows{(*cave).size()};
     auto const ncols{(*cave).front().size()};
-    std::vector<bool> unique_positions;
-    unique_positions.resize(nrows*ncols, false);
-    size_t t = 0;
+    seen.resize(nrows*ncols);
+    for (auto s: seen) {s.reset();};
 
     while (beam_fronts.size() > 0) { 
-        t++;
         BeamFront bf = std::move(beam_fronts.back());
         beam_fronts.pop_back();
-        seen.insert(bf);
-        unique_positions[bf.i + bf.j * ncols] = true;
+        seen[bf.i + bf.j * ncols].set((size_t) bf.dir);
         for (auto d: descendents(cave, bf))
         {
-            if (d.i >= 0 && d.i < ncols && d.j >= 0 && d.j < nrows && !seen.contains(d)) beam_fronts.push_back(d);
+            if (d.i >= 0 && d.i < ncols && d.j >= 0 && d.j < nrows && 
+                !seen[d.i+d.j*ncols][(size_t) d.dir]) beam_fronts.push_back(d);
         }
     }
     
-    return std::count(unique_positions.begin(), unique_positions.end(), true);
+    size_t t = 0;
+    for (auto b: seen) {t += (size_t) b.any();};
+    return t;
 }
 
 int main() {
@@ -142,22 +149,22 @@ int main() {
     {
         cave.push_back(line);
     }
-    auto sol_part1 = propagate_beam(&cave, BeamFront{0, 0, '>'});
+    auto sol_part1 = propagate_beam(&cave, BeamFront{0, 0, Direction::right});
     std::cout << "Part 1 solution: " << sol_part1 << "\n";
 
     size_t sol_part2 = 0;
     for (int j = 0; j < cave.size(); j++)
     {
-        sol_part2 = std::max(sol_part2, propagate_beam(&cave, BeamFront{0, j, '>'}));
-        sol_part2 = std::max(sol_part2, propagate_beam(&cave, BeamFront{(int) cave[0].size() - 1, j, '<'}));
+        sol_part2 = std::max(sol_part2, propagate_beam(&cave, BeamFront{0, j, Direction::right}));
+        sol_part2 = std::max(sol_part2, propagate_beam(&cave, BeamFront{(int) cave[0].size() - 1, j, Direction::left}));
     };
     for (int i = 0; i < cave[0].size(); i++)
     {
-        sol_part2 = std::max(sol_part2, propagate_beam(&cave, BeamFront{i, 0, 'v'}));
-        sol_part2 = std::max(sol_part2, propagate_beam(&cave, BeamFront{i, (int) cave.size() - 1, '^'}));
+        sol_part2 = std::max(sol_part2, propagate_beam(&cave, BeamFront{i, 0, Direction::down}));
+        sol_part2 = std::max(sol_part2, propagate_beam(&cave, BeamFront{i, (int) cave.size() - 1, Direction::up}));
     };
 
-    std::cout << "Part 1 solution: " << sol_part2 << "\n";
+    std::cout << "Part 2 solution: " << sol_part2 << "\n";
     return 0; 
 }
 
