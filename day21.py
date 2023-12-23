6c261c41412498972247721e12ac3e30f2a91f13
Guilherme Saturnino <guilherme.bicalho2@gmail.com>
2023-12-23 14:41:54 +0100

    day21, pt2

--- day21.py
+++ day21.py
@@ -17,7 +17,8 @@ EXAMPLE = """
 
 def solve():
     input_file_contents = open(os.path.join("input", "day21")).read().rstrip()
-    input_file_contents = EXAMPLE
+    #input_file_contents = open(os.path.join("input", "day21_expanded")).read().rstrip()
+    #input_file_contents = EXAMPLE
 
     region_map = input_file_contents.splitlines()
     start = None
@@ -26,11 +27,26 @@ def solve():
             start = (i, j)
 
 
-    sol_part1 = can_reach(region_map, start, 6)
+    sol_part1 = can_reach(region_map, start, 64)
     print("Part 1:", sol_part1)
 
-    sol_part2 = can_reach_infinite(region_map, start, 5000)
-    print("Part 2:", sol_part2)
+    x = [0, 1, 2, 3, 4]
+    y = [
+        can_reach_infinite(region_map, start, 65 + 131 * xi)
+        for xi in x
+    ]
+
+    # quadratic fit
+    c = y[0]
+    b = (y[1] * 4 - y[2] - 3*c) // 2
+    a = y[1] - b - c
+    assert all(yi == a * xi**2 + b * xi + c for yi, xi in zip(y, x))
+    # extrapolate
+    target = 26501365
+    x_t = (target - 65) // 131
+    assert (target - 65) % 131 == 0
+    print("Part 2:", a * x_t**2 + b * x_t + c)
+
 
 
 def can_reach(region_map, start_pos, n_steps):
@@ -43,8 +59,8 @@ def can_reach(region_map, start_pos, n_steps):
                 if 0 <= ni < mi and 0 <= nj < mj and region_map[nj][ni] in ".S":
                     next_positions.add((ni, nj))
         positions = next_positions
-        print(i+1, len(positions))
 
+    #open("tmp", "w").write(map_str(region_map, positions))
     return len(positions)
 
 
@@ -59,20 +75,18 @@ def can_reach_infinite(region_map, start_pos, n_steps):
             for ni, nj in [(pi + 1, pj), (pi - 1, pj), (pi, pj + 1), (pi, pj - 1)]:
                 if region_map[nj % mj][ni % mi] in ".S":
                     next_step.add((ni, nj))
-        #breakpoint()
         next_edge = next_step - prev_edge
         bulk[i % 2] += len(prev_edge)
         prev_edge = edge
         edge = next_edge
-        if i%100 == 0:
-            print(len(edge))
 
     return bulk[i % 2] + len(edge)
 
 
-def print_map(region_map, positions):
-    for j, row in enumerate(region_map):
-        print("".join("O" if (i, j) in positions else c for i, c in enumerate(row)))
+def map_str(region_map, positions):
+    return "\n".join(
+        "".join("O" if (i, j) in positions else c for i, c in enumerate(row))
+        for j, row in enumerate(region_map))
 
 
 if __name__ == "__main__":
